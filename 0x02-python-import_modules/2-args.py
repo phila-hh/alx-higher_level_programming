3df5acc2a34c6b36f5359312a925d765584492b6
phila-hh <filimon.haftomh@gmail.com>
2023-06-08 10:36:12 -0700

    Fixed some more mistakes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     argLength = len(argv) - 1
     argString = "argument"
-    if argLength > 1:
+    if argLength != 1:
         argString += "s"
 
     print(f"{argLength} {argString}" + ("." if argLength == 0 else ":"))
