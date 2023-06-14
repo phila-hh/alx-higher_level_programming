773826372882835feadfb0a80aaf923bd73fe50d
phila-hh <filimon.haftomh@gmail.com>
2023-06-14 11:52:49 -0700

    fixed some errors

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for i in my_list:
-        new_list.append(i)
-    new_list[new_list.index(search)] = replace
-    return new_list
+    return [replace if n == search else n for n in my_list]
