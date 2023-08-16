82be3f1a943769e93e2f3954b32fed16f5d3076d
phila-hh <filimon.haftomh@gmail.com>
2023-08-16 12:34:57 -0700

    fixed some more mistakes

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- user_0d_1 gets all privileges on the MySQL server
 -- Sets user_0d_1 password to user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON * . * TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
