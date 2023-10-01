b17f00b67ddf4937db246b48cdd296e4dd74c685
phila-hh <filimon.haftomh@gmail.com>
2023-10-01 06:23:05 -0700

    fix: edited all the bash scripts to containd only 3 lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
-
-response=$(curl -s -w "%{http_code}" "$1")
-
-if [ "$response" -eq 200 ]; then
-	curl -s "$1" | sed -n '/<body>/,/<\/body>/p'
-fi
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL "$1"
